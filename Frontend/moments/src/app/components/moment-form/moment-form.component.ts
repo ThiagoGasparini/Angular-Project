29507522526319d66ff56ac45a0698de3f039ace
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-06 19:46:16 -0300

    validação de formulário com angular

--- Frontend/moments/src/app/components/moment-form/moment-form.component.ts
+++ Frontend/moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,14 +1,45 @@
-import { Component, Input } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './moment-form.component.html',
-  styleUrl: './moment-form.component.css'
+  styleUrl: './moment-form.component.css',
 })
-export class MomentFormComponent {
-  @Input() btnText!: string
+export class MomentFormComponent implements OnInit {
+  @Input() btnText!: string;
 
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log('Enviou formulário');
+  }
 }
