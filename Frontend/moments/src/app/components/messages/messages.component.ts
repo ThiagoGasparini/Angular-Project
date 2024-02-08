2b3edce1e5cdea1e6debd363dc7f2e5fe94f26a0
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-08 20:16:22 -0300

    iniciando sistema de mensagem

---
+++ Frontend/moments/src/app/components/messages/messages.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MessagesService } from '../../services/messages.service';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css',
+})
+export class MessagesComponent {
+  faTimes = faTimes;
+
+  constructor(public messagesService: MessagesService) {}
+}
