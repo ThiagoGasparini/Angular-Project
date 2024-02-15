04d7f54c32a05643172ed67414cf8df5851849dc
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-15 15:10:28 -0300

    buscando dados da api e renderizando na pagina home

--- Frontend/moments/src/app/components/pages/home/home.component.ts
+++ Frontend/moments/src/app/components/pages/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { MomentService } from '../../../services/moment.service';
+import { Moment } from '../../../Moment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl: string = 'http://localhost:3333/';
 
+  constructor(private momentService: MomentService) {
+    this.getMoments();
+  }
+
+  getMoments(): void {
+    this.momentService.getMoments().subscribe((response) => {
+      const data = response.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
