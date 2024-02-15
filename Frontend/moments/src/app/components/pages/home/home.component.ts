029e10d1abaf236eb8f264fd46b5f2702a83bf2e
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-15 15:58:51 -0300

    criando input de busca e filtrando os dados da api

--- Frontend/moments/src/app/components/pages/home/home.component.ts
+++ Frontend/moments/src/app/components/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { MomentService } from '../../../services/moment.service';
 import { Moment } from '../../../Moment';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -15,6 +16,9 @@ export class HomeComponent {
   allMoments: Moment[] = [];
   moments: Moment[] = [];
   baseApiUrl: string = 'http://localhost:3333/';
+
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) {
     this.getMoments();
@@ -25,11 +29,22 @@ export class HomeComponent {
       const data = response.data;
 
       data.map((item) => {
-        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
       });
 
       this.allMoments = data;
       this.moments = data;
+    });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value);
     });
   }
 }
